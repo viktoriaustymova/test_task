93f6a7338c377f6ebf585cc96989e05f5bda2a22
Viktoriia Ustymova <svi.3481@gmail.com>
2024-03-04 19:00:04 +0100

    modules 2 and 3 added

--- js/modules.js
+++ js/modules.js
@@ -1,44 +1,40 @@
 const toggleButton = document.getElementById('more-button');
 const hiddenMenuIcons = document.querySelector('.hidden-menu');
-const firstMenuItem = document.querySelector('.modules-list-item-first');
-const secondItem = document.querySelector('.modules-list li:nth-child(2)');
 const hiddenIcon = document.querySelector('.hidden-icon');
+const hiddenItem = document.querySelector('.modules-list li:nth-child(4)');
 const hiddenSeparator = document.querySelector('.hidden-separator');
 const hiddenMenuItems = document.querySelectorAll('.modules-list .hidden');
-const thirdMenuItem = document.querySelector('.modules-list li:nth-child(3)');
 const navbar = document.querySelectorAll('.module-list-container');
 const menuLinks = document.querySelectorAll('.menu-links');
 const modulesList = document.querySelectorAll('.modules-list li');
-
+const selectedMenuItem = document.querySelector('.modules-list-item-selected');
 
 toggleButton.addEventListener('click',toggleSideMenu);
 
 function toggleSideMenu() {
-   hiddenMenuIcons.classList.toggle('visible');
-   secondItem.style.display = 'none';
-   hiddenIcon.style.display = 'none';
-   hiddenSeparator.style.display = 'none';
-   thirdMenuItem.style.borderTop = '1px solid rgba(176, 176, 176, 1)';
-   hiddenMenuItems.forEach(item => item.classList.remove('hidden'));
-   modulesList.forEach(item => {
-      item.onmouseover = () => {
-         item.style.backgroundColor = 'rgba(243, 243, 243, 1)';
-         item.style.borderRadius = '3px';
-         item.style.border = '0px';
-      };
-
-      item.onmouseout = () => {
-         item.style.backgroundColor = '';
-         item.style.borderRadius = '';
-         item.style.border = '';
-      };
-   });
-   hiddenMenuItems.forEach(item => item.style.fontStyle = 'italic');
+    hiddenMenuIcons.classList.toggle('visible');
+    hiddenItem.style.display = 'none';
+    hiddenIcon.style.display = 'none';
+    hiddenSeparator.style.display = 'none';
+    hiddenMenuItems.forEach(item => item.classList.remove('hidden'));
+    modulesList.forEach(item => {
+       item.onmouseover = () => {
+          item.style.backgroundColor = 'rgba(243, 243, 243, 1)';
+          item.style.borderRadius = '3px';
+          item.style.border = '0px';
+       };
+ 
+       item.onmouseout = () => {
+          item.style.backgroundColor = '';
+          item.style.borderRadius = '';
+          item.style.border = '';
+       };
+    });
 }
 
 let previousTarget = null;
-let firstMenuIcon = document.querySelector('.first-icon');
-let newFirstMenuIcon = 'icons/dotBlack.svg';
+let selectedSecondIcon = document.querySelector('.selected-icon');
+let newSelectedMenuIcon = 'icons/dotBlack.svg';
 let firstMenuIconDefault = 'icons/dot.svg';
 let menuItemIcons = document.querySelectorAll('.hidden-menu .module-list-icon');
 let selectedMenuIcon = 'icons/LockedOrange.svg';
@@ -54,10 +50,10 @@ function toggleItem(event){
       previousTarget.style.color = 'rgba(0, 0, 0, 1)';
       previousTarget.style.fontWeight = 300;
       previousTarget.style.fontStyle = 'italic';
-      firstMenuItem.style.color = 'rgba(0, 0, 0, 1)';
-      firstMenuItem.style.fontWeight = 300;
-      firstMenuItem.style.fontStyle = 'normal';
-      firstMenuIcon.src = newFirstMenuIcon;
+      selectedMenuItem.style.color = 'rgba(0, 0, 0, 1)';
+      selectedMenuItem.style.fontWeight = 300;
+      selectedMenuItem.style.fontStyle = 'normal';
+      selectedSecondIcon.src = newSelectedMenuIcon;
    }
    
    event.target.style.fontStyle = 'normal';
@@ -73,9 +69,7 @@ function toggleItem(event){
   
    previousTarget = event.target;
 
-   if(event.target === firstMenuItem){
-      firstMenuIcon.src = firstMenuIconDefault;
+   if(event.target === selectedMenuItem){
+      selectedSecondIcon.src = firstMenuIconDefault;
    }
 }
-
-
